892f8045c63dc5f101a311b8a19142d595b0c2a7
Shree <Shree@DESKTOP-9SF6INJ>
2025-02-28 09:48:25 +0530

    logger CM

--- CreditManager_CM/src/main/java/in/cm/controller/CmController.java
+++ CreditManager_CM/src/main/java/in/cm/controller/CmController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,46 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import in.cm.model.Customer;
 import in.cm.service.CMserviceI;
-
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/cm")
+@Slf4j
 public class CmController {
 
 	@Autowired
 	CMserviceI cmi;
-	
-	
+
 	@GetMapping("/get")
 	public String log() {
 		return "success!";
 	}
-		
-		@PutMapping("/addsanction/{CustomerId}")
-		public ResponseEntity<Customer> updatesanction(@PathVariable("CustomerId") int CustomerId, @RequestBody Customer cs)
-		{
-			
-			cmi.updateSanction(CustomerId,cs);
-			
-			return null;
-			
-		}
-			
-			
-			
-			
-			
-			
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	@PutMapping("/addsanction/{CustomerId}")
+	public ResponseEntity<Customer> updatesanction(@PathVariable("CustomerId") int CustomerId,
+			@RequestBody Customer cs) {
+
+		cmi.updateSanction(CustomerId, cs);
+
+		log.error("this is error log");
+		log.info("this is info log");
+		log.warn("this is warn log");
+		log.debug("this is debug log");
+
+		return null;
+
 	}
 
+}
